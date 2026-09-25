c4638633458cb8a5f8d6d32e257524a58f32cbef
S0MEB0DYY <eng.adamabouelrous@gmail.com>
2026-09-26 02:47:19 +0300

    Neural Network
    Added loss calculation using LossFunction and updated print statement to display loss.

--- Python/Neural Network/neuralNetwork.py
+++ Python/Neural Network/neuralNetwork.py
@@ -2,12 +2,15 @@ import numpy
 import nnfs
 from layer import *
 from activation import *
+from loss import *
 
 nnfs.init()
 
 X = [[0.5, 3.49, 0.21, 2],
      [1, 4.94, 2.01, 1.5],
      [0.6, 2.3, 1.7, 0.1]]
+
+y = [0, 1, 1]
 
 layer1 = Layer(4, 5)
 layer1.forward(X)
@@ -24,5 +27,7 @@ layer3.forward(activation2.output)
 activation3 = ActivationSoftmax()
 activation3.forward(layer3.output)
 
-print(activation3.output)
+lossFunction = LossFunction()
+loss = lossFunction.calculate(activation3.output, y)
 
+print("Loss: ", loss)
