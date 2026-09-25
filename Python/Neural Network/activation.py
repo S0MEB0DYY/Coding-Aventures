c95f31be36eaceebb3219048e7d13ef39bf8e3a3
S0MEB0DYY <eng.adamabouelrous@gmail.com>
2026-09-26 01:51:58 +0300

    Neural Network

--- Python/Neural Network/activation.py
+++ Python/Neural Network/activation.py
@@ -5,5 +5,5 @@ class ActivationReLU:
     self.output = np.maximum(0, inputs)
 class ActivationSoftmax:
   def forward(self, inputs):
-    expValues = np.exp(inputs) - np.max(inputs, axis=1, keepdims=True)
+    expValues = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
     self.output = expValues / np.sum(expValues, axis=1, keepdims=True)
