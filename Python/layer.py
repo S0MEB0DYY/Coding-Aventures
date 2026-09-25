e24b55f46bed196ac5c1be735e2b8aebf5cb44de
S0MEB0DYY <eng.adamabouelrous@gmail.com>
2026-09-25 17:20:01 +0300

    Neural Network

---
+++ Python/layer.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class Layer:
+  def __init__(self, n_inputs, n_neurons):
+    self.weights = np.random.randn((n_inputs, n_neurons))
+    self.biases = np.zeros((1, n_neurons))
+  def forward(self, inputs):
+    self.output = np.dot(inputs, self.weights) + biases
